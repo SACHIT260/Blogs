f2da030026c84797bd4831bea73abd84d749d7a9
SACHIT260 <96931281+SACHIT260@users.noreply.github.com>
2024-08-21 14:32:56 +0530

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        fetch('https://blogs-nx1t.onrender.com/blogs')
             .then(res => {
                 if (!res.ok) {
                     throw Error('Could not fetch the data for that resource');
